223db13c71ee875e3927f0f66b69bbd7d4c2d999
Amirmansi <163479758+Amirmansi@users.noreply.github.com>
2024-08-07 13:32:20 +0300

    Update test_device_log.py

--- zk_integration/zk/doctype/device_log/test_device_log.py
+++ zk_integration/zk/doctype/device_log/test_device_log.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2021, Peter and Contributors
-# See license.txt
-from __future__ import unicode_literals
 
-# import frappe
-import unittest
-
-class TestDeviceLog(unittest.TestCase):
-	pass
