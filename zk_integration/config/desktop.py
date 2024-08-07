f03adc80dbe50111c97f8c7775283652249d4670
Amirmansi <163479758+Amirmansi@users.noreply.github.com>
2024-08-07 13:05:18 +0300

    Update desktop.py

--- zk_integration/config/desktop.py
+++ zk_integration/config/desktop.py
@@ -1,14 +1,1 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from frappe import _
 
-def get_data():
-	return [
-		{
-			"module_name": "ZK",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("ZK")
-		}
-	]
